2175cca5477d6099bdce9a684a4954c3be141d8d
unknown <softdev@SI-203-06.harding.edu>
2015-02-04 09:40:59 -0600

    - Initial Upload

---
+++ New Unity Project/Assets/Scripts/core.cs
@@ -0,0 +1,129 @@
+using UnityEngine;
+using System.Collections;
+using System.Text;
+using System.IO;
+
+public class core : MonoBehaviour {
+
+	public Material solid;
+	private bool firstMove;
+	public GamePlace[,] gamePlaces;
+	public string turn;
+	private int redLastCol;
+	private int redLastRow;
+	private int redLastPiece;
+	private int blackLastCol;
+	private int blackLastRow;
+	private int blackLastPiece;
+	private int turnsLeft;
+
+	// Use this for initialization
+	void Start () {
+        turnsLeft = 56;
+		firstMove = true;
+		turn = "red";
+		StreamReader reader = new StreamReader ("gameBoard.txt");
+		string[] pieceNumbers = reader.ReadToEnd().Split(',');
+		//Debug.Log (pieceNumbers);
+		gamePlaces = new GamePlace[8,8];
+		for (int i = 0; i < 8; i++) {
+			for (int k = 0; k < 8; k++) {
+				GamePlace gp = new GamePlace();
+				//Debug.Log(gp);
+				Debug.Log(pieceNumbers[(8 * i) + k]);
+				//System.Console.Write(pieceNumbers[(8 * i) + k]);
+				gp.pieceNum = int.Parse(pieceNumbers[(8 * i) + k]);
+				gp.owner = "open";
+				//Debug.Log(gp);
+				gamePlaces[i,k] =  gp;
+			}
+		}
+		for (int i = 0; i < 8; i++) {
+			for (int k = 0; k < 8; k++) {
+				Debug.Log(gamePlaces[i,k].pieceNum);
+				Debug.Log(gamePlaces[i,k].owner);
+			}
+			Debug.Log('\n');
+		}
+	}
+
+	public void PlacePiece (test sender) {
+		gamePlaces [sender.boardX, sender.boardY].owner = turn;
+        sender.gameObject.renderer.enabled = true;
+		sender.gameObject.renderer.material = solid;
+		if (turn == "red") {
+			sender.gameObject.renderer.material.color = Color.red;
+			redLastCol = sender.boardX;
+			redLastRow = sender.boardY;
+			redLastPiece = sender.pieceNum;
+			turn = "black";
+		} else {
+			sender.gameObject.renderer.material.color = Color.black;
+			blackLastCol = sender.boardX;
+			blackLastRow = sender.boardY;
+			blackLastPiece = sender.pieceNum;
+			turn = "red";
+		}
+        turnsLeft--;
+		char winner = checkForWin ();
+	}
+
+    public void ShowLight(test sender)
+    {
+        Light light = GameObject.Find("TestLight").GetComponent<Light>();
+
+        light.light.intensity = 5;
+    }
+
+	char checkForWin() {
+		char winner = '*';
+		bool gameOver = false;
+		if (turn == "black" && turnsLeft == 0) {
+			gameOver = true;
+		}
+		if (noValidMove())
+			gameOver = true;
+		return winner;
+	}
+
+	bool noValidMove() {
+        bool result = true;
+		for (int i = 0; i < 8; i++) {
+			for (int k = 0; k < 8; k++) {
+                if (isValidMove(i, k))
+                    result = false;
+			}
+		}
+        return result;
+	}
+
+	public bool isValidMove(int x, int y){
+		bool result = true;
+		Debug.Log (gamePlaces [y, x].pieceNum);
+		Debug.Log (redLastPiece);
+		if (firstMove == true) {
+						firstMove = false;
+		} else {
+			if (gamePlaces [y, x].pieceNum == redLastPiece || 
+			    gamePlaces [y, x].pieceNum == blackLastPiece) {
+				result = false;
+			}
+			if (turn == "red" && x != blackLastCol && y != blackLastRow)
+				result = false;
+
+			if (turn == "black" && x != redLastCol && y != redLastRow) 
+				result = false;
+
+			if (gamePlaces [y, x].pieceNum == redLastPiece ||
+			    gamePlaces [y, x].pieceNum == blackLastPiece) {
+				result = false;
+			}
+		}
+		return result;
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ New Unity Project/Assets/Scripts/test.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour {
+
+
+	public int boardX;
+	public int boardY;
+	public int pieceNum;
+	private core gameCore;
+
+	// Use this for initialization
+	void Start () {
+		gameCore = GameObject.Find ("GameCore").GetComponent<core>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnMouseDown(){
+		Debug.Log (boardX);
+		Debug.Log (boardY);
+		if (gameCore.isValidMove(boardX, boardY)) {
+			gameCore.PlacePiece(this);
+			gameObject.collider.enabled = false;
+		}
+	}
+
+    void OnMouseOver()
+    {
+        if(gameCore.isValidMove(boardX, boardY))
+        {
+            gameCore.ShowLight(this);
+        }
+    }
+}
