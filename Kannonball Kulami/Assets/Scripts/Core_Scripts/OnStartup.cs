2d5ca0c5fd7d3204656b8e71dc12f2c7cf2d850d
A3mercury <aandrew1@harding.edu>
2015-02-23 19:05:17 -0600

    OnStartup in the gamecore can be used as the start of the game. Currently set resolution to 1440x900

---
+++ Kannonball Kulami/Assets/Scripts/Core_Scripts/OnStartup.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnStartup : MonoBehaviour 
+{
+    void Awake()
+    {
+        Debug.Log("Awake!");
+
+        //Resolution[] resolutions = Screen.resolutions;
+        //foreach(Resolution res in resolutions)
+        //{
+        //    print(res.width + "x" + res.height);
+        //}
+
+        Screen.SetResolution(1440, 900, true);
+    }
+}
