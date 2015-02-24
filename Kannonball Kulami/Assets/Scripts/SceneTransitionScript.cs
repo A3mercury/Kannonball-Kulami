45b99d4eb7cc0491a0b95813e0e19fbddda7c306
Carver Petty <cpetty1@harding.edu>
2015-02-24 11:47:42 -0600

    Added Main Menu, Options, and Credits scenes. Added scene transitions between them and the gameboardTestScene.

---
+++ Kannonball Kulami/Assets/Scripts/SceneTransitionScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneTransitionScript : MonoBehaviour {
+
+	public void SinglePlayer () {
+		Application.LoadLevel("gameboardTestScene");
+	}
+	
+	public void NetworkPlay () {
+		Application.LoadLevel("gameboardTestScene");
+	}
+
+	public void Options () {
+		Application.LoadLevel ("OptionsScene");
+	}
+
+	public void Credits () {
+		Application.LoadLevel("CreditsScene");
+	}	
+
+	public void MainMenu () {
+		Application.LoadLevel("MainMenuScene");
+	}
+
+	public void OnMouseDown () {
+		if (gameObject.name.ToString () == "MenuCube") 
+		{
+			MainMenu ();
+		}
+	}
+}
