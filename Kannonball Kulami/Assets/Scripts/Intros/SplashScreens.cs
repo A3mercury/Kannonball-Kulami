ee4e559abe8a0c14ffa673c498a0cb7ec4253fee
A3mercury <aandrew1@harding.edu>
2015-03-01 23:23:09 -0600

    Added intro splash screens

---
+++ Kannonball Kulami/Assets/Scripts/Intros/SplashScreens.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplashScreens : MonoBehaviour
+{
+    private bool firstLogo;
+
+    GameObject ssLogo;
+    GameObject kkLogo;
+
+	// Use this for initialization
+	void Start () 
+    {
+        firstLogo = true;
+
+        ssLogo = GameObject.Find("sslogo");
+        ssLogo.SetActive(true);
+
+        kkLogo = GameObject.Find("kklogo");
+        kkLogo.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if(firstLogo)
+            {
+                ssLogo.SetActive(false);
+                kkLogo.SetActive(true);
+                firstLogo = false;
+            }
+            else
+            {
+                Application.LoadLevel("MainMenuS");
+            }
+        }
+	}
+
+
+}
