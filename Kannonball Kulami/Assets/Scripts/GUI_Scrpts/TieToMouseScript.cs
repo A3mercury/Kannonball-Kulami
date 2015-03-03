95290ee769da627eb210065210d9cdaa56e0efa6
Carver Petty <cpetty1@harding.edu>
2015-03-03 12:35:33 -0600

    Added TieToMouseScript, which currently ties a cube to the mouse (Actually working!). Removed FollowMouseScript because it is now unused. Tweaked LookAtMouseScript, but am still having some issues with it so it has not yet been applied to the cannons.

---
+++ Kannonball Kulami/Assets/Scripts/GUI_Scrpts/TieToMouseScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TieToMouseScript : MonoBehaviour {
+
+	private float speed = 100.0f;
+	private Vector3 position;
+
+	// Use this for initialization
+	void Start () {
+		position = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//float distance = -transform.position.y + Camera.main.transform.position.y;
+
+		position = new Vector3((Input.mousePosition.x/8) - 47, Input.mousePosition.z, (Input.mousePosition.y/6) - 25);
+		//position = Camera.main.ScreenToWorldPoint(position);
+
+		position.y = 15;
+
+		transform.position = Vector3.MoveTowards (transform.position, position, speed * Time.deltaTime);
+	}
+}
