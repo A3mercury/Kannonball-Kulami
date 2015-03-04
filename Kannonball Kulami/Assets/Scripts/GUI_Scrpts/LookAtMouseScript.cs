3892b542b0ede22cc750b94411f4bdbe17d55395
Carver Petty <cpetty1@harding.edu>
2015-03-04 10:47:09 -0600

    Couple small changes to LookAtMouseScript.

--- Kannonball Kulami/Assets/Scripts/GUI_Scrpts/LookAtMouseScript.cs
+++ Kannonball Kulami/Assets/Scripts/GUI_Scrpts/LookAtMouseScript.cs
@@ -4,15 +4,10 @@ using System.Collections;
 public class LookAtMouseScript : MonoBehaviour {
 
 	public Transform target;
-	public Transform self;
-	public float speed = 1.0f;
 		
 	void Update () 
 	{
-		Vector3 targetDir = target.position - transform.position;
-		float step = speed * Time.deltaTime;
-		Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, step, 0.0F);
-		Debug.DrawRay(transform.position, newDir, Color.red);
-		self.GetChild(0).transform.rotation = Quaternion.LookRotation(newDir);
+		Vector3 relativePos = target.position - transform.position;
+		transform.rotation = Quaternion.LookRotation(relativePos);
 	}
 }
