4c4252db76a401e7f68fd7aa6145bfa131485bef
A3mercury <aandrew1@harding.edu>
2015-03-15 21:05:37 -0500

    changed some names of files and folders.

--- Kannonball Kulami/Assets/Scripts/GUI_Scrpts/OptionsScript.cs
+++ Kannonball Kulami/Assets/Scripts/GUI_Scrpts/OptionsScript.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class OptionsScript : MonoBehaviour 
 {
     public static bool isOptionsOpen = false;
+
 	// Use this for initialization
 	void Start () 
     {
@@ -13,6 +14,16 @@ public class OptionsScript : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
     {
-	
+	    
 	}
+
+    public void ResumeApplication()
+    {
+
+    }
+
+    public void QuitApplication()
+    {
+        Application.Quit();
+    }
 }
--- Kannonball Kulami/Assets/Scripts/GUI_Scrpts/SceneTransitionScript.cs
+++ Kannonball Kulami/Assets/Scripts/GUI_Scrpts/SceneTransitionScript.cs
@@ -2,18 +2,38 @@
 using System.Collections;
 
 public class SceneTransitionScript : MonoBehaviour {
+    public bool optionsMenu = false;
+    public GameObject optionsCanvas;
+
+    void Start()
+    {
+        //optionsCanvas = GameObject.Find("OptionsCanvas");
+        optionsCanvas = GameObject.FindGameObjectWithTag("Options");
+        //optionsCanvas.SetActive(false);
+    }
 
 	public void SinglePlayer () {
         Network_Manager.fromtransition = false;
-		Application.LoadLevel("gameboardTestScene");
+		Application.LoadLevel("GameScene");
 	}
 	
 	public void NetworkPlay () {
         Network_Manager.fromtransition = true;
-		Application.LoadLevel("gameboardTestScene");
+		Application.LoadLevel("GameScene");
 	}
 
 	public void Options () {
+        if (!optionsCanvas.activeSelf)
+        {
+            optionsMenu = true;
+            optionsCanvas.SetActive(true);
+        }
+        else
+        {
+            optionsMenu = false;
+            optionsCanvas.SetActive(false);
+        }
+
 		//Application.LoadLevel ("OptionsScene");
 	}
 
@@ -22,7 +42,7 @@ public class SceneTransitionScript : MonoBehaviour {
 	}	
 
 	public void MainMenu () {
-		Application.LoadLevel("MainMenuS");
+		Application.LoadLevel("MainMenuScene");
 	}
 
     public void ExitGame()
@@ -30,33 +50,34 @@ public class SceneTransitionScript : MonoBehaviour {
         Application.Quit();
     }
 
-    //public void OnMouseDown () {
-    //    if (!optionsMenu)
-    //    {
-    //        if (gameObject.name.ToString() == "MenuCube")
-    //        {
-    //            MainMenu();
-    //        }
+    public void OnMouseDown()
+    {
+        if (!optionsMenu)
+        {
+            if (gameObject.name.ToString() == "MenuCube")
+            {
+                MainMenu();
+            }
 
-    //        if (gameObject.name.ToString() == "singleplayer")
-    //        {
-    //            SinglePlayer();
-    //        }
+            if (gameObject.name.ToString() == "singleplayer")
+            {
+                SinglePlayer();
+            }
 
-    //        if (gameObject.name.ToString() == "networkplay")
-    //        {
-    //            NetworkPlay();
-    //        }
+            if (gameObject.name.ToString() == "networkplay")
+            {
+                NetworkPlay();
+            }
 
-    //        if (gameObject.name.ToString() == "options")
-    //        {
-    //            Options();
-    //        }
+            if (gameObject.name.ToString() == "options")
+            {
+                Options();
+            }
 
-    //        if (gameObject.name.ToString() == "credits")
-    //        {
-    //            Credits();
-    //        }
-    //    }
-    //}
+            if (gameObject.name.ToString() == "credits")
+            {
+                Credits();
+            }
+        }
+    }
 }
