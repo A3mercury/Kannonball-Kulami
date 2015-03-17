b616d49a6b7abf80e6d1105c3de4af2168812009
Carver Petty <cpetty1@harding.edu>
2015-03-17 11:39:15 -0500

    Fixed the TieToMouseScript. It now follows the mouse exactly.

--- Kannonball Kulami/Assets/Scripts/GUI_Scrpts/TieToMouseScript.cs
+++ Kannonball Kulami/Assets/Scripts/GUI_Scrpts/TieToMouseScript.cs
@@ -3,23 +3,17 @@ using System.Collections;
 
 public class TieToMouseScript : MonoBehaviour {
 
-	private float speed = 75.0f;
-	private Vector3 position;
+	private float actualDistance;
 
 	// Use this for initialization
 	void Start () {
-		position = transform.position;
+		actualDistance = (transform.position - Camera.main.transform.position).magnitude;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		//float distance = -transform.position.y + Camera.main.transform.position.y;
-
-		position = new Vector3((Input.mousePosition.x/8) - 47, Input.mousePosition.z, (Input.mousePosition.y/6) - (78/3));
-		//position = Camera.main.ScreenToWorldPoint(position);
-
-		position.y = 15;
-
-		transform.position = Vector3.MoveTowards (transform.position, position, speed * Time.deltaTime);
+		Vector3 myMousePosition = Input.mousePosition;
+		myMousePosition.z = actualDistance;
+		transform.position = Camera.main.ScreenToWorldPoint (myMousePosition);
 	}
 }
