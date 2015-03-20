d540e95ab4f86dbd6973a6713a7d1eb64ab35f38
A3mercury <aandrew1@harding.edu>
2015-03-20 11:32:20 -0500

    Figured out how to animate the buttons to switch sprites on the activation of a bool. On the invitation dialog, accept works when bool checkbox is turned on and off.

--- Kannonball Kulami/Assets/Scripts/GUI_Scrpts/OverrideOnClickForButtons.cs
+++ Kannonball Kulami/Assets/Scripts/GUI_Scrpts/OverrideOnClickForButtons.cs
@@ -6,11 +6,13 @@ using System.IO;
 public class OverrideOnClickForButtons : MonoBehaviour 
 {
     Button acceptButton;
+    Sprite acceptButtonPressed;
 
 	// Use this for initialization
 	void Start () 
     {
         acceptButton = this.GetComponent<Button>();
+        acceptButtonPressed = new Sprite();
 	}
 
     void OnClick()
