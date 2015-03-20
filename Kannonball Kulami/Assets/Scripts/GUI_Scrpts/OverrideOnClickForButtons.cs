182b3adbc81618e055a6a31faea3f06cc9c03068
A3mercury <aandrew1@harding.edu>
2015-03-20 10:51:29 -0500

    no message

---
+++ Kannonball Kulami/Assets/Scripts/GUI_Scrpts/OverrideOnClickForButtons.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.IO;
+
+public class OverrideOnClickForButtons : MonoBehaviour 
+{
+    Button acceptButton;
+
+	// Use this for initialization
+	void Start () 
+    {
+        acceptButton = this.GetComponent<Button>();
+	}
+
+    void OnClick()
+    {
+
+    }
+}
