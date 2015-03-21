58e53d6f18067ab6b4365e459c61d1aa8aab24e6
A3mercury <aandrew1@harding.edu>
2015-03-20 23:42:30 -0500

    Chat box opens and closes with the use of the pullout tab. I then found an easier way to sprite swap and I'm a little bummed I didn't find it much sooner -.-

---
+++ Kannonball Kulami/Assets/Scripts/GUI_Scrpts/ChatBoxUI.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ChatBoxUI : MonoBehaviour 
+{
+    Animator[] chatboxAnimators;
+    Animator ChatOpenCloseAnimator;
+    Animator SendButtonAnimator;
+
+    public Button[] chatboxButtons;
+    public Button sendChatButton;
+    public Button pulloutTab;
+
+	// Use this for initialization
+	void Start () 
+    {
+        // assigning the animators
+        chatboxAnimators = GetComponentsInChildren<Animator>();
+        foreach(Animator anim in chatboxAnimators)
+        {
+            if(anim.runtimeAnimatorController.name == "ChatOpenCloseController")
+            {
+                ChatOpenCloseAnimator = anim;
+                ChatOpenCloseAnimator.SetBool("isChatOpen", false);
+                Debug.Log("Is this running?");
+            }
+            else if(anim.runtimeAnimatorController.name == "SendButtonController")
+            {
+                SendButtonAnimator = anim;
+                SendButtonAnimator.SetBool("isSend", false);
+            }
+        }
+
+        // assigning the buttons
+        chatboxButtons = GetComponentsInChildren<Button>();
+        foreach(Button button in chatboxButtons)
+        {
+            if (button.name == "send_button")
+                sendChatButton = button;
+            if (button.name == "pullout_tab")
+                pulloutTab = button;
+        }
+
+        // event listeners for the buttons
+        sendChatButton.onClick.AddListener(SendMessage);
+        pulloutTab.onClick.AddListener(OpenCloseChat);
+	}
+
+    // Called when the pulloutTab is clicked
+    public void OpenCloseChat()
+    {
+        if (ChatOpenCloseAnimator.GetBool("isChatOpen"))
+            ChatOpenCloseAnimator.SetBool("isChatOpen", false);
+        else
+            ChatOpenCloseAnimator.SetBool("isChatOpen", true);
+    }
+
+    public void SendMessage()
+    {
+        // access the animator to change the button when pressed
+        SendButtonAnimator.SetBool("isSend", true);
+        
+
+        // will send message to chat
+    }
+}
