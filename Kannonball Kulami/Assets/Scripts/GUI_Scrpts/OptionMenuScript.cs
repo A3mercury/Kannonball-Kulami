9dd463ef429d0c8c4581ac0352807a7bab2999bd
Carver Petty <cpetty1@harding.edu>
2015-03-21 11:51:37 -0500

    Separated the activation/deactivation functionality into a new script, OpenOptions, which got it all working. Discovered how to block clicks past the options canvas, but it needs to be implemented in the game core script that controls game board clicks, so ill show Sam what I've found instead of wrecking his code.

--- Kannonball Kulami/Assets/Scripts/GUI_Scrpts/OptionMenuScript.cs
+++ Kannonball Kulami/Assets/Scripts/GUI_Scrpts/OptionMenuScript.cs
@@ -3,14 +3,13 @@ using UnityEngine.UI;
 using System.Collections;
 
 public class OptionMenuScript : MonoBehaviour {
-
-	public Canvas optionsCanvas;
+	
 	public AudioSource soundSource;
 	public Slider soundSlider;
 
 	void Start ()
 	{
-		//optionsCanvas.enabled = false;
+
 	}
 
 	public void concedeGame ()
@@ -23,28 +22,8 @@ public class OptionMenuScript : MonoBehaviour {
 		Application.Quit();
 	}
 
-	public void resumeGame ()
-	{
-		optionsCanvas.enabled = false;
-	}
-
 	public void soundSliderChange ()
 	{
 		soundSource.volume = soundSlider.value;
-	}
-
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetKeyDown (KeyCode.Escape)) 
-		{
-			if (optionsCanvas.enabled == true)
-			{
-				optionsCanvas.enabled = false;
-			}
-			else
-			{
-				optionsCanvas.enabled = true;
-			}
-		}
 	}
 }
