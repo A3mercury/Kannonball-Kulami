3484e920edf5a35d7c82ef579a86ba04267bdc5b
Sam Hipp <samhipp93@gmail.com>
2015-03-24 00:25:58 -0500

    Changed how buoyancy works.  Now it doesn't do anything crazy, it just makes things float nice and easy.  I'll get it worked into the main scene tomorrow morning (unless Carver's stuff ends up doing it better than mine, I haven't actually seen it yet...). Anyway, Austin, don't bother changing the stuff that we had talked about, I figured out the real problem.

---
+++ Kannonball Kulami/Assets/Scripts/Attached.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Attached : MonoBehaviour {
+	private Vector3 attachedPosition;
+
+	// Use this for initialization
+	void Start () {
+		attachedPosition = transform.localPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.localPosition = attachedPosition;
+	}
+}
