baf59af62cbb9487867cd94e7845ea9211aaa4bb
Carver Petty <cpetty1@harding.edu>
2015-03-25 16:11:03 -0500

    Added start up information to the main game scene. It comes up 3 seconds after the level loads, stays up for 10 seconds, then disappears.

---
+++ Kannonball Kulami/Assets/Scripts/GUI_Scrpts/GameScenePopUpInfo.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameScenePopUpInfo : MonoBehaviour {
+
+	private bool doGameStartInfo = false;
+	// Use this for initialization
+	void Start () {
+		OnLevelWasLoaded ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnLevelWasLoaded ()
+	{
+		StartCoroutine (gameStartInfo ());
+	}
+
+	IEnumerator gameStartInfo ()
+	{
+		yield return new WaitForSeconds(3);
+
+		doGameStartInfo = true;
+	}
+
+	IEnumerator endGameStartInfo ()
+	{
+		yield return new WaitForSeconds (10);
+
+		doGameStartInfo = false;
+	}
+
+	void DoWindow0(int windowID) {
+		
+	}
+
+	void OnGUI ()
+	{
+		if (doGameStartInfo)
+			GUI.Window(0, new Rect(500, 10, 200, 80), DoWindow0, "You go first this game. Place a piece anywhere on the board to begin.");	
+
+		StartCoroutine (endGameStartInfo ());
+	}
+}
