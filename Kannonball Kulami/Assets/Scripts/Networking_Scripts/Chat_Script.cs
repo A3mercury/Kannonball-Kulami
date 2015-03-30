28ea64343277cc1b1ac1dd15edf728ae6cd51e67
unknown <softdev@SI-203-06.harding.edu>
2015-03-30 12:43:27 -0500

    Worked on accept request box

--- Kannonball Kulami/Assets/Scripts/Networking_Scripts/Chat_Script.cs
+++ Kannonball Kulami/Assets/Scripts/Networking_Scripts/Chat_Script.cs
@@ -6,11 +6,7 @@ public class Chat_Script : MonoBehaviour
     Network_Manager get;
     void Awake()
     {
-        get = GameObject.Find("Network_Manager").GetComponent<Network_Manager>();
-        //myStyle = new GUIStyle();
-        
-        //myOtherStyle = new GUIStyle();
-        
+        get = GameObject.Find("Network_Manager").GetComponent<Network_Manager>();        
     }
 
     public Vector2 scrollPosition;
@@ -23,24 +19,24 @@ public class Chat_Script : MonoBehaviour
 
     private void OnGUI()
     {
-            GUI.skin = myskin;
-            if (get.isInGame)
-            {
-                if (firstConnection)
-                {
-                    if (Network.isServer)
-                    {
-                        messBox = get.clientName + " has challenged you to a game! You will go second, and play as black.\n";
-                    }
-                    else
-                    {
-                        messBox = "You have connected to " + get.serverName + ". You will go first, and play as red.\n";
-                    }
-                    firstConnection = false;
-                }
+            //GUI.skin = myskin;
+            //if (get.isInGame)
+            //{
+            //    if (firstConnection)
+            //    {
+            //        if (Network.isServer)
+            //        {
+            //            messBox = get.clientName + " has challenged you to a game! Do you accept?\n";
+            //        }
+            //        else
+            //        {
+            //            messBox = "You have connected to " + get.serverName + ". You will go first, and play as red.\n";
+            //        }
+            //        firstConnection = false;
+            //    }
                 
-                windowRect = GUI.Window(1, windowRect, windowFunc, "Chat");
-            }
+            //    windowRect = GUI.Window(1, windowRect, windowFunc, "Chat");
+            //}
             
     }
 
--- Kannonball Kulami/Assets/Scripts/Networking_Scripts/Send_Move_Over_Network.cs
+++ Kannonball Kulami/Assets/Scripts/Networking_Scripts/Send_Move_Over_Network.cs
@@ -3,13 +3,78 @@ using System.Collections;
 
 public class Send_Move_Over_Network : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
+    public Vector2 scrollPosition;
+    private GUIStyle myStyle;
+    public GUISkin myskin;
+    private GUIStyle myOtherStyle;
+    Network_Manager get;
+    private Rect windowRect = new Rect((Screen.width - 250)/2, (Screen.height - 100) / 2, 250, 100);
+    private string messBox = "", messageToSend = "", user = "";
+    private bool firstConnection = true;
+    void Awake()
+    {
+        get = GameObject.Find("Network_Manager").GetComponent<Network_Manager>();
+    }
 	// Update is called once per frame
-	void Update () {
-	
-	}
+    private void OnGUI()
+    {
+        GUI.skin = myskin;
+        if(get.isInGame)
+        {
+            if(firstConnection)
+            {
+                if(Network.isServer)
+                {
+                    messBox = get.clientName + " has challenged you to a game! Do you accept?\n";
+                }
+                else
+                {
+                    messBox = "You have challenged " + get.serverName + " to a game. Awaiting response...\n";
+                }
+                firstConnection = false;
+            }
+            windowRect = GUI.Window(1, windowRect, windowFunc, "");
+        }
+    }
+
+    private void windowFunc(int id)
+    {
+        myStyle = GUI.skin.box;
+        myStyle.alignment = TextAnchor.UpperLeft;
+        myStyle.wordWrap = true;
+        myStyle.stretchHeight = true;
+        myOtherStyle = GUI.skin.textField;
+        myOtherStyle.alignment = TextAnchor.MiddleLeft;
+        myOtherStyle.clipping = TextClipping.Clip;
+        myOtherStyle.wordWrap = false;
+        myOtherStyle.fixedWidth = 200;
+
+        scrollPosition = GUILayout.BeginScrollView(scrollPosition, GUILayout.Width(280));
+        GUILayout.Box(messBox, myStyle);
+        GUILayout.EndScrollView();
+        GUILayout.BeginHorizontal();
+        //messageToSend = GUILayout.TextField(messageToSend, myOtherStyle);
+        if (GUILayout.Button("Accept", GUILayout.Width(75)))
+        {
+
+            //networkView.RPC("SendMyMessage", RPCMode.All, get.userName + ": " + messageToSend + "\n");
+            //Debug.Log(messBox);
+            //messageToSend = "";
+        }
+        if(GUILayout.Button("Deny", GUILayout.Width(75)))
+        {
+        
+        }
+        GUILayout.EndHorizontal();
+        GUILayout.BeginHorizontal();
+        GUILayout.EndHorizontal();
+        
+    }
+    [RPC]
+    private void SendMyMessage(string mess)
+    {
+        Debug.Log(mess);
+        messBox += mess;
+    }
+   
 }
