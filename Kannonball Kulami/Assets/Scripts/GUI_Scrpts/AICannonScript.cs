a701002c19a72970a2d025b6d01b9809c9af79f2
Carver Petty <cpetty1@harding.edu>
2015-04-01 11:11:29 -0500

    Increased the board size and made it where the board doesn't drop into position when the game starts.

---
+++ Kannonball Kulami/Assets/Scripts/GUI_Scrpts/AICannonScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AICannonScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
