25c0dd2675234c152b0dd925bb487dfed6a11e11
unknown <softdev@SI-203-06.harding.edu>
2015-04-05 19:43:29 -0500

    Chat is sending messages, need to have it show up in box.

--- Kannonball Kulami/Assets/Scripts/Networking_Scripts/Chat_Script.cs
+++ Kannonball Kulami/Assets/Scripts/Networking_Scripts/Chat_Script.cs
@@ -4,10 +4,10 @@ using UnityEngine.UI;
 
 public class Chat_Script : MonoBehaviour
 {
-    Network_Manager get;
+    Network_Manager networkManager;
     void Awake()
     {
-        get = GameObject.Find("Network_Manager").GetComponent<Network_Manager>();        
+        networkManager = GameObject.Find("Network_Manager").GetComponent<Network_Manager>();        
     }
 
     public Vector2 scrollPosition;
@@ -15,7 +15,7 @@ public class Chat_Script : MonoBehaviour
     private GUIStyle myStyle;
     private GUIStyle myOtherStyle;
     private bool firstConnection = true;
-    private Rect windowRect = new Rect(0, 275, 300, 200);
+    private Rect windowRect;
     private string messBox = "", messageToSend = "", user = "";
 
     // Chat canvas stuff
@@ -23,75 +23,99 @@ public class Chat_Script : MonoBehaviour
     public GameObject ChatBoxPanel;
     public Button sendButton;
     public InputField messageField;
-    public Image pulloutTab;
+    public Button pulloutTab;
 
     public Animator ChatBoxShowHide;
-    public Animator SendMessageButton;
+
+    public GUISkin ChatSkin;
 
     void Start()
     {
-        //sendButton.onClick.AddListener(SendButtonClick);
+        
+
+        sendButton.onClick.AddListener(SendButtonClick);
+        pulloutTab.onClick.AddListener(OpenCloseChat);
     }
 
-    //private void OnGUI()
-    //{
-            //GUI.skin = myskin;
-            //if (get.isInGame)
+    void Update()
+    {
+
+    }
+
+    public void OpenCloseChat()
+    {
+        if (ChatBoxShowHide.GetBool("isChatOpen"))
+            ChatBoxShowHide.SetBool("isChatOpen", false);
+        else
+            ChatBoxShowHide.SetBool("isChatOpen", true);
+    }
+
+    private void OnGUI()
+    {
+        GUI.skin = ChatSkin;
+
+        windowRect = new Rect(
+            (Screen.width * 2f) / 100, 
+            (Screen.height * 34f) / 100, 
+            (Screen.width * 24f) / 100, 
+            (Screen.height * 50f) / 100
+            );
+
+        //if (networkManager.isInGame)
+        //{
+            //if (firstConnection)
             //{
-            //    if (firstConnection)
+            //    if (Network.isServer)
             //    {
-            //        if (Network.isServer)
-            //        {
-            //            messBox = get.clientName + " has challenged you to a game! Do you accept?\n";
-            //        }
-            //        else
-            //        {
-            //            messBox = "You have connected to " + get.serverName + ". You will go first, and play as red.\n";
-            //        }
-            //        firstConnection = false;
+            //        messBox = networkManager.clientName + " has challenged you to a game! Do you accept?\n";
             //    }
-                
-            //    windowRect = GUI.Window(1, windowRect, windowFunc, "Chat");
+            //    else
+            //    {
+            //        messBox = "You have connected to " + networkManager.serverName + ". You will go first, and play as red.\n";
+            //    }
+            //    firstConnection = false;
             //}
-            
-    //}
+            if(ChatBoxShowHide.GetBool("isChatOpen"))
+                GUI.Window(120, windowRect, windowFunc, "");
+        //}
+
+    }
 
     void SendButtonClick()
     {
-        if(messageField.text != "")
-            SendMyMessage(messageField.text);
+        if (messageField.text != "")
+        {
+            networkView.RPC("SendMyMessage", RPCMode.All, networkManager.userName + ": " + messageField.text + "\n");
+            messageField.text = "";
+        }
     }
 
     private void windowFunc(int id)
     {
-        myStyle = GUI.skin.box;
-        myStyle.alignment = TextAnchor.UpperLeft;
-        myStyle.wordWrap = true;
-        myStyle.stretchHeight = true;
-        myOtherStyle = GUI.skin.textField;
-        myOtherStyle.alignment = TextAnchor.MiddleLeft;
-        myOtherStyle.clipping = TextClipping.Clip;
-        myOtherStyle.wordWrap = false;
-        myOtherStyle.fixedWidth = 200;
+        //myStyle = GUI.skin.box;
+        //myStyle.alignment = TextAnchor.UpperLeft;
+        //myStyle.wordWrap = true;
+        //myStyle.stretchHeight = true;
+        //myOtherStyle = GUI.skin.textField;
+        //myOtherStyle.alignment = TextAnchor.MiddleLeft;
+        //myOtherStyle.clipping = TextClipping.Clip;
+        //myOtherStyle.wordWrap = false;
+        //myOtherStyle.fixedWidth = 200;
         //GUILayout.Box(messBox, myStyle, GUILayout.Height(350));
-        scrollPosition = GUILayout.BeginScrollView(scrollPosition, GUILayout.Width(280));
-        GUILayout.Box(messBox, myStyle);
+
+        GUI.skin = ChatSkin;
+
+        scrollPosition = GUILayout.BeginScrollView(scrollPosition);
+        GUILayout.Box(messBox, ChatSkin.box);
         GUILayout.EndScrollView();
-        GUILayout.BeginHorizontal();
-        messageToSend = GUILayout.TextField(messageToSend, myOtherStyle);
-        if (GUILayout.Button("Send", GUILayout.Width(75)))
-        {
-            
-            networkView.RPC("SendMyMessage", RPCMode.All, get.userName + ": " + messageToSend + "\n");
-            Debug.Log(messBox);
-            messageToSend = "";
-        }
-        GUILayout.EndHorizontal();
-        GUILayout.BeginHorizontal();
-        //GUILayout.Label("User:");
-        //user = GUILayout.TextField(user);
-        GUILayout.EndHorizontal();
-        GUI.DragWindow(new Rect(0, 0, Screen.width, Screen.height));
+        //GUILayout.BeginHorizontal();
+
+        //GUILayout.EndHorizontal();
+        //GUILayout.BeginHorizontal();
+        ////GUILayout.Label("User:");
+        ////user = GUILayout.TextField(user);
+        //GUILayout.EndHorizontal();
+        //GUI.DragWindow(new Rect(0, 0, Screen.width, Screen.height));
     }
 
    [RPC]
