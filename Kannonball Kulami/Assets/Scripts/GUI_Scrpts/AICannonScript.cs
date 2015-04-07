ef1722aa40c8dd2c3f9e092e2ecd02858e10d1b9
Sam Hipp <samhipp93@gmail.com>
2015-04-06 22:23:18 -0500

    AI cannon turns to fire now instead of snapping to the new place.  I think I can add the same functionality to the cannons over the network if we want. Or really, I can change the way the cannons move to make it work over the network and Caleb can actually send the data (I could break everything in the networking, and we don't want that).

--- Kannonball Kulami/Assets/Scripts/GUI_Scrpts/AICannonScript.cs
+++ Kannonball Kulami/Assets/Scripts/GUI_Scrpts/AICannonScript.cs
@@ -4,9 +4,17 @@ using System.Collections;
 public class AICannonScript : MonoBehaviour {
 
 	GameObject OpponentCannon;
-	Vector3 shotPosition;
+	Vector3 targetPosition = new Vector3(-14f, 11f, -14f);
+	Vector3 oldPosition;
 	private GameCore coreScript;
-
+	public float speed = 1.0F;
+	private float startTime;
+	private float journeyLength;
+	private bool firing = false;
+	private bool firstMove = true;
+	private int targetRow;
+	private int targetCol;
+	
 	// Use this for initialization
 	void Start () {
 		OpponentCannon = gameObject;//GameObject.Find("OpponentCannon");
@@ -15,9 +23,37 @@ public class AICannonScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(coreScript.AIMove != null)
-			MoveCannon (coreScript.AIMove.transform.position);
-			//MoveCannon(new Vector3(10f, 10f, 5f));
+		//if(coreScript.myJob != null && coreScript.AIMove != null)
+		//{
+		//	//MoveCannon (coreScript.AIMove.transform.position);
+		//	//MoveCannon(new Vector3(10f, 10f, 5f));
+		//	oldPosition = targetPosition;
+		//	targetPosition = coreScript.AIMove.transform.position;
+		//	startTime = Time.time;
+		//	journeyLength = Vector3.Distance(oldPosition, targetPosition);
+		//}
+		if(!firstMove)
+		{
+			float distCovered = (Time.time - startTime) * speed;
+			float fracJourney = distCovered / journeyLength;
+			if(fracJourney >= 1)
+			{
+				//Debug.Log ("done moving cannon");
+				if(firing)
+				{
+					Debug.Log("firing cannon");
+					//FIRE!
+					coreScript.PlacePiece(targetRow, targetCol);
+					firing = false;
+				}
+			}
+			else
+			{
+				OpponentCannon.transform.LookAt(Vector3.Lerp(oldPosition, targetPosition, fracJourney));
+				OpponentCannon.transform.eulerAngles = new Vector3(0, OpponentCannon.transform.eulerAngles.y + 90, 0);
+			}
+		}
+
 	}
 
 	void MoveCannon(Vector3 target)
@@ -25,5 +61,17 @@ public class AICannonScript : MonoBehaviour {
 		OpponentCannon.transform.LookAt(target);
 		//Debug.Log("Euler angle x: " + PlayerCannon.transform.eulerAngles.x + " y: " + PlayerCannon.transform.eulerAngles.y + 90 + " z: " + PlayerCannon.transform.eulerAngles.z);
 		OpponentCannon.transform.eulerAngles = new Vector3(0, OpponentCannon.transform.eulerAngles.y + 90, 0);
+	}
+
+	public void MoveCannonAndFire(Vector3 target, int row, int col)
+	{
+		targetRow = row;
+		targetCol = col;
+		oldPosition = targetPosition;
+		targetPosition = target;
+		startTime = Time.time;
+		journeyLength = Vector3.Distance(oldPosition, targetPosition);
+		firing = true;
+		firstMove = false;
 	}
 }
