211d7cf06623eb23a4d7ce443e80fe01627f04e3
Sam Hipp <samhipp93@gmail.com>
2015-04-10 09:54:58 -0500

    Added an treasure chest and coins from the asset store into the main game scene.  You can click on the gold pieces and little coins fly out with a nice jingly sound.  Also added some other assets that might come in handy. (jewels and gold and such)

---
+++ Kannonball Kulami/Assets/clickCoins.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class clickCoins : MonoBehaviour {
+
+    public Transform spoutTransform;
+    public ParticleSystem coinSpout;
+    public AudioSource coinSound;
+   // public BoxCollider mesh;
+   // public SphereCollider sphere;
+    private Camera mainCam;
+
+    void Start()
+    {
+        mainCam = GameObject.Find("Main Camera").camera;
+		spoutTransform = GameObject.Find("Coin Spout").transform;
+        //coinSpout = GetComponentInChildren<ParticleSystem>();
+        //mesh = GetComponent<MeshCollider>();
+        //sphere = GetComponent<SphereCollider>();
+    }
+
+	void OnMouseDown()
+    {
+		spoutTransform.position = collider.bounds.center;
+		//Debug.Log("clicked");
+		coinSpout.Play();
+        coinSound.PlayOneShot(coinSound.clip);
+        //RaycastHit hit;
+        //Physics.Raycast(mainCam.ScreenPointToRay(Input.mousePosition), out hit);
+        //Vector3 closestSphere = sphere.ClosestPointOnBounds(hit.point);
+        //Vector3 closestMesh = mesh.ClosestPointOnBounds(hit.point);
+        ////if (hit.collider.name == "Sphere Collider" )
+        //{
+        //    spoutTransform.position = closestSphere;
+		//	Debug.Log(closestSphere + "sphere");
+        //    coinSpout.Play();
+        //}
+        //else
+        //{
+        //    spoutTransform.position = closestMesh;
+        //    coinSpout.Play();
+		//	Debug.Log(closestMesh + "mesh");
+        //}
+    }
+}
---
+++ Kannonball Kulami/Assets/openChest.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class openChest : MonoBehaviour {
+    public Animator openAnimator;
+	public MeshCollider top;
+	public MeshCollider keyHole;
+	void OnMouseDown()
+    {
+        openAnimator.SetTrigger("isOpen");
+		top.enabled = false;
+		keyHole.enabled = false;
+    }
+}
